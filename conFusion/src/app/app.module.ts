4e5dce2e86340f4943dec7e5d8d126f482d3fac6
Sarmistha Sarkar <sarmisthasarkar93@gmail.com>
2022-02-18 21:15:57 +0530

    Components Part 1
    Components Part 1

--- conFusion/src/app/app.module.ts
+++ conFusion/src/app/app.module.ts
@@ -6,16 +6,20 @@ import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import {MatToolbarModule} from '@angular/material/toolbar';
 import { FlexLayoutModule } from '@angular/flex-layout';
+import { MenuComponent } from './menu/menu.component';
+import {MatListModule} from '@angular/material/list';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MenuComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-    MatToolbarModule,FlexLayoutModule
+    MatToolbarModule,FlexLayoutModule,
+    MatListModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ conFusion/src/app/shared/dish.ts
@@ -0,0 +1,11 @@
+export class Dish{
+    id: string | undefined;
+    name: string | undefined;
+    image: string | undefined;
+    category: string | undefined;
+    featured: boolean | undefined;
+    label: string | undefined;
+    price: string | undefined;
+    description: string | undefined;
+    
+}
