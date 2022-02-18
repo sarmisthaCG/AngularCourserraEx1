f9e8de9a385fa0495350d68c58563c72c3033bad
Sarmistha Sarkar <sarmisthasarkar93@gmail.com>
2022-02-18 21:28:54 +0530

    Components Part 2
    Components Part 2

--- conFusion/src/app/app.module.ts
+++ conFusion/src/app/app.module.ts
@@ -8,6 +8,9 @@ import {MatToolbarModule} from '@angular/material/toolbar';
 import { FlexLayoutModule } from '@angular/flex-layout';
 import { MenuComponent } from './menu/menu.component';
 import {MatListModule} from '@angular/material/list';
+import { MatGridListModule } from '@angular/material/grid-list';
+import { MatCardModule } from '@angular/material/card';
+import { MatButtonModule } from '@angular/material/button';
 
 @NgModule({
   declarations: [
@@ -19,7 +22,8 @@ import {MatListModule} from '@angular/material/list';
     AppRoutingModule,
     BrowserAnimationsModule,
     MatToolbarModule,FlexLayoutModule,
-    MatListModule
+    MatListModule,MatGridListModule,
+    MatCardModule,MatButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
