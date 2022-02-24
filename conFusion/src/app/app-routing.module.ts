cb3c82911826df7cc1e5f0ad7e2348e14c05837f
Sarmistha Sarkar <sarmisthasarkar93@gmail.com>
2022-02-24 18:07:37 +0530

    Single Page Applications Part 1
    Single Page Applications Part 1

--- conFusion/src/app/app-routing.module.ts
+++ conFusion/src/app/app-routing.module.ts
@@ -1,12 +1,14 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ContactComponent } from './contact/contact.component';
 import { HomeComponent } from './home/home.component';
 import { MenuComponent } from './menu/menu.component';
 
 const routes: Routes = [
   { path: 'home',  component: HomeComponent },
   { path: 'menu',     component: MenuComponent },
+  { path: 'contactus',     component: ContactComponent },
   { path: '', redirectTo: '/home', pathMatch: 'full' }
 ];
 
